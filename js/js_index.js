d3845c70bb7c4e0fe15338a73f9fa05fb0a9c04c
LeQuangMinh2602 <142397660+LeQuangMinh2602@users.noreply.github.com>
2023-08-16 21:59:42 +0700

    Add files via upload

---
+++ js/js_index.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+    let num = $(".slider > div").length;
+    let h = "";
+    let log = $(".btnLogin").innerText;
+    for (let i = 0; i < num; i++)
+        h += `
+            <button class="digit">${i + 1}</button>
+        `;
+        $(".buttons").html(h);
+    $(".slider").height($(".slider img").height());
+
+    $(window).scroll(function stick(){
+        if($(this).scrollTop())
+        {
+            $("header").addClass("sticky")
+        }
+        else
+        {
+            $("header").removeClass("sticky")
+        }
+    })
+});
