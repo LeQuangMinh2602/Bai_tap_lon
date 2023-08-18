d61551a89c16072d973494a19b60c05041600cc5
Tropic-Skull <128499518+Tropic-Skull@users.noreply.github.com>
2023-08-18 22:29:11 +0700

    slider

--- js/js_index.js
+++ js/js_index.js
@@ -1,13 +1,39 @@
 $(document).ready(() => {
+    let current=0;
     let num = $(".slider > div").length;
     let h = "";
     let log = $(".btnLogin").innerText;
-    for (let i = 0; i < num; i++)
-        h += `
-            <button class="digit">${i + 1}</button>
-        `;
-        $(".buttons").html(h);
-    $(".slider").height($(".slider img").height());
+
+    let showSlider= (current)=> {
+        $(".slider > div").fadeOut("slow");
+        $(".slider > div").eq(current).fadeIn("slow");
+    }
+
+    $(".buttons button.digit").click(function(){
+        let idx=parseInt($(this).text());
+        current=idx-1;
+        showSlider(current); 
+    });
+    
+    $(".next").click(()=>{
+        current++;
+        if(current ===num)
+            current=0;
+        showSlider(current);
+        
+        clearInterval(timer,100);
+        
+    });
+    
+    $(".prev").click(()=>{
+        current--;
+        if(current < 0)
+            current=num - 1;
+        showSlider(current);
+        
+        clearInterval(timer,100);
+        
+    });
 
     $(window).scroll(function stick(){
         if($(this).scrollTop())
@@ -19,4 +45,15 @@ $(document).ready(() => {
             $("header").removeClass("sticky")
         }
     })
+
+    let timer =null;
+    let runSlider= (duration) => {
+        setInterval(()=>{
+            $(".next").click();
+        },duration)
+    }
+
+    runSlider(5000);
+
+    $(".slider").height($(".slider img").height());
 });
